40533f47e201bdb6d036e2ab2535780ab620d2b6
Michelle Morilhas <michelle.morilhas@gmail.com>
2022-07-15 10:30:34 -0300

    criacao calculadora

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -13,3 +13,39 @@ public class PrimeiroPrograma {
         System.out.println("Hello World!" + (a+b));*/
     }
 }
+
+class Livro {
+    public String nome;
+    public Integer npags;
+
+    public Livro(String nome, Integer npags) {
+        this.nome = nome;
+        this.npags = npags;
+    }
+
+    public Livro() {     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpags() {
+        return npags;
+    }
+
+    public void setNpags(Integer npags) {
+        this.npags = npags;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npags=" + npags +
+                '}';
+    }
+}
